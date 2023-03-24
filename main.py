1a0cac3d156e000fbd02122338168da47229b0a0
Nicandro <nikpote@hotmail.it>
2023-03-24 14:16:04 +0100

    addedo preprocessing

---
+++ main.py
@@ -0,0 +1,3 @@
+# @Authors: Filippi Francesco - francesco.filippi4@studio.unibo.it - 0001059730, 
+#           Potena Nicandro - nicandro.potena@studio.unibo.it - 0001000827
+# @Date: 01/05/2023 (gg/mm/aaaa)
