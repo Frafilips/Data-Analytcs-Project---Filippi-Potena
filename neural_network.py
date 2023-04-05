f1a2f496034fe8905f019e51d6318722b28f623a
Nicandro <nikpote@hotmail.it>
2023-04-05 18:06:17 +0200

    added testing

--- neural_network.py
+++ neural_network.py
@@ -56,11 +56,11 @@ class Feedforward(torch.nn.Module):
         self.device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         dropout = 0.2
         self.model = torch.nn.Sequential(
-            #torch.nn.Dropout(dropout),
+            torch.nn.Dropout(dropout),
             torch.nn.Linear(input_size, hidden_size),
             torch.nn.ReLU(),
             torch.nn.BatchNorm1d(hidden_size),
-            #torch.nn.Dropout(dropout),
+            torch.nn.Dropout(dropout),
             torch.nn.Linear(hidden_size, hidden_size),
             torch.nn.ReLU(),
             torch.nn.BatchNorm1d(hidden_size),
@@ -92,7 +92,28 @@ def train_model(model, criterion, optimizer, epochs, data_loader,device):
 
     return model, loss_values
 
-def test_model(model, data_loader,best_accuracy,device,parameters,best_parameters):
+def val_model(model, data_loader,best_accuracy,device,parameters,best_parameters):
+    model.eval()
+    y_pred = []
+    y_val = []
+    for data, targets in data_loader:
+        data, targets = data.to(device), targets.to(device)
+        y_pred.append(model(data))
+        y_val.append(targets)
+    y_pred = torch.stack(y_pred).squeeze().cpu()
+    y_val = torch.stack(y_val).squeeze().cpu()
+    y_pred = y_pred.argmax(dim=1, keepdim=True).squeeze().cpu()
+    score = torch.sum((y_pred.squeeze() == y_val).float()) / y_val.shape[0]
+    #print('Test score', score.numpy())
+    print(classification_report(y_val, y_pred,zero_division=0))
+    accuracy=accuracy_score(y_val, y_pred)
+    print("accuracy: ",accuracy)
+    if(accuracy>best_accuracy):
+        best_accuracy=accuracy
+        best_parameters=parameters
+    return best_accuracy,best_parameters
+
+def test_model(model, data_loader,device):
     model.eval()
     y_pred = []
     y_test = []
@@ -103,15 +124,11 @@ def test_model(model, data_loader,best_accuracy,device,parameters,best_parameter
     y_pred = torch.stack(y_pred).squeeze().cpu()
     y_test = torch.stack(y_test).squeeze().cpu()
     y_pred = y_pred.argmax(dim=1, keepdim=True).squeeze().cpu()
-    """score = torch.sum((y_pred.squeeze() == y_test).float()) / y_test.shape[0]
-    print('Test score', score.numpy())"""
-    #print(classification_report(y_test, y_pred,zero_division=0))
+    score = torch.sum((y_pred.squeeze() == y_test).float()) / y_test.shape[0]
+    #print('Test score', score.numpy())
+    print(classification_report(y_test, y_pred,zero_division=0))
     accuracy=accuracy_score(y_test, y_pred)
-    print(accuracy)
-    if(accuracy>best_accuracy):
-        best_accuracy=accuracy
-        best_parameters=parameters
-    return best_accuracy,best_parameters
+    print("Accuracy: ",accuracy)
 
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -119,24 +136,27 @@ if __name__ == "__main__":
 
     best_accuracy=0
     hidden_sizes = [16, 32,64]
-    nums_epochs = [10, 20]
+    nums_epochs = [10, 20,50]
     batch = [16, 32,64]
     learning_rate = [0.01,0.001]
-    parameters=[]
+    momentum=[0.9,0.95,0.99]
     best_parameters=[]
-    hyperparameters = itertools.product(hidden_sizes, nums_epochs, batch,learning_rate)
+    hyperparameters = itertools.product(hidden_sizes, nums_epochs, batch,learning_rate,momentum)
 
     datasetTrain=MovieLens(x_train, y_train)
     datasetTest=MovieLens(x_test, y_test)
     datasetVal=MovieLens(x_val, y_val)
 
     val_loader=DataLoader(datasetVal, batch_size=1, shuffle=True)
+    test_loader=DataLoader(datasetTest, batch_size=1, shuffle=True)
     
-    for hidden_size, num_epochs, batch, learning_rate in hyperparameters:
+    for hidden_size, num_epochs, batch, learning_rate, momentum in hyperparameters:
+        parameters=[]
         parameters.append(hidden_size)
         parameters.append(num_epochs)
         parameters.append(batch)
         parameters.append(learning_rate)
+        parameters.append(momentum)
 
         torch.manual_seed(42)
         np.random.seed(42)
@@ -145,19 +165,49 @@ if __name__ == "__main__":
         model = Feedforward(x_train.shape[1], hidden_size, datasetTrain.num_classes)
         model.to(device)
         criterion = torch.nn.CrossEntropyLoss() #Softmax and NNLL, does not require one-hot encoding of labels
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=0.9)
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=momentum)
 
         #test_model(model,val_loader, device)
         model, loss_values = train_model(model, criterion, optimizer, num_epochs, train_loader, device)
-        best_accuracy,best_parameters=test_model(model, val_loader,best_accuracy, device,parameters,best_parameters)
-        print("hiddensize: ",hidden_size,"num epochs: ",num_epochs,"batch: ",batch,"learning rate: ",learning_rate)
+        print("Hiddensize: ",hidden_size,"; Num epochs: ",num_epochs,"; Batch: ",batch,"; Learning rate: ",learning_rate,"; Momentum: ",momentum)
+        best_accuracy,best_parameters=val_model(model, val_loader,best_accuracy, device,parameters,best_parameters)
+        
         plt.clf()
         plt.plot(loss_values)
         plt.title("Number of epochs: {}".format(num_epochs))
         #plt.show()
 
     print("Best accuracy: ",best_accuracy)
-    print("Best parameters: ",best_parameters)
+    #Training with best parameters and testing the model
+    best_hidden_size=best_parameters[0]
+    best_num_epochs=best_parameters[1]
+    best_batch=best_parameters[2]
+    best_learning_rate=best_parameters[3]
+    best_momentum=best_parameters[4]
+    print("Best parameters: hidden_size:",best_hidden_size,"; num_epochs: ",best_num_epochs,"; batch: ",best_batch,"; learning_rate: ",best_learning_rate,": momentum: ",best_momentum)
+    train_loader=DataLoader(datasetTrain, batch_size=best_batch, shuffle=True,drop_last=True)
+    model = Feedforward(x_train.shape[1], best_hidden_size, datasetTrain.num_classes)
+    model.to(device)
+    criterion = torch.nn.CrossEntropyLoss() #Softmax and NNLL, does not require one-hot encoding of labels
+    optimizer = torch.optim.SGD(model.parameters(), lr=best_learning_rate,momentum=best_momentum)
+    model, loss_values = train_model(model, criterion, optimizer, best_num_epochs, train_loader, device)
 
-    #hiddensize:  16 num epochs:  20 batch:  64 learning rate:  0.01
-    #0.9493304379297864
+    plt.clf()
+    plt.plot(loss_values)
+    plt.title("Number of epochs: {}".format(best_num_epochs))
+    #plt.show()
+    test_model(model, test_loader, device)
+
+    #Salvataggio modello
+    print("Salvataggio modello")
+    torch.save(model.state_dict(), 'nn_model.pt')
+
+    # Carica il modello salvato
+    print("Carico il modello")
+    model = Feedforward(x_train.shape[1], best_hidden_size, datasetTrain.num_classes)
+    model.to(device)
+    model.load_state_dict(torch.load('nn_model.pt'))
+    
+    
+    # Utilizza il modello per fare predizioni sui tuoi dati di test
+    test_model(model, test_loader, device)
